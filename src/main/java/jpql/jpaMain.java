1e7dbf68a5940ac80db94249e4dbdd909efcb285
cares0 <cares00000@gmail.com>
2022-06-04 15:52:57 +0900

    조인

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -14,26 +14,22 @@ public class jpaMain {
         tx.begin();
 
         try {
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.changeTeam(team);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(50) //50번째 부터
-                    .setMaxResults(10) //10개의 데이터를 가져온다
+            String query = "select m from Member m inner join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-
-            System.out.println("result.size() = " + result.size());
-            for (Member findMember : result) {
-                System.out.println("findMember = " + findMember);
-            }
-
 
             tx.commit();
         } catch (Exception e) {
